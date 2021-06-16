95d9068ccf0828d9220f3b99d53cc86f61421aac
David Langendoen <david@electricfunstuff.com>
2021-06-16 17:33:06 -0400

    add M2 streaming sfx

--- Assets/Muse/Game/Data/Audio/AudioIds.cs
+++ Assets/Muse/Game/Data/Audio/AudioIds.cs
@@ -1,5 +1,17 @@
 //CLASS AudioIds
 public partial class AudioIds {
+    //PROPERTY auction_2
+    public const string auction_2 = "auction_2";
+
+    //PROPERTY bell
+    public const string bell = "bell";
+
+    //PROPERTY come_in
+    public const string come_in = "come_in";
+
+    //PROPERTY crickets
+    public const string crickets = "crickets";
+
     //PROPERTY demogame_0
     public const string demogame_0 = "demogame_0";
 
@@ -32,6 +44,204 @@ public partial class AudioIds {
 
     //PROPERTY demogame_9
     public const string demogame_9 = "demogame_9";
+
+    //PROPERTY horse_approach
+    public const string horse_approach = "horse_approach";
+
+    //PROPERTY horse_recede
+    public const string horse_recede = "horse_recede";
+
+    //PROPERTY knock_1
+    public const string knock_1 = "knock_1";
+
+    //PROPERTY knock_2
+    public const string knock_2 = "knock_2";
+
+    //PROPERTY long_john_loop
+    public const string long_john_loop = "long_john_loop";
+
+    //PROPERTY lucy_epilogue
+    public const string lucy_epilogue = "lucy_epilogue";
+
+    //PROPERTY lucy_epilogue_intro
+    public const string lucy_epilogue_intro = "lucy_epilogue_intro";
+
+    //PROPERTY lucy_epilogue_pause
+    public const string lucy_epilogue_pause = "lucy_epilogue_pause";
+
+    //PROPERTY lucy_hungry
+    public const string lucy_hungry = "lucy_hungry";
+
+    //PROPERTY lucy_i_made_it_e
+    public const string lucy_i_made_it_e = "lucy_i_made_it_e";
+
+    //PROPERTY lucy_moan_b
+    public const string lucy_moan_b = "lucy_moan_b";
+
+    //PROPERTY lucy_on_own
+    public const string lucy_on_own = "lucy_on_own";
+
+    //PROPERTY mob_small
+    public const string mob_small = "mob_small";
+
+    //PROPERTY mus_sfx_axe_on_metal
+    public const string mus_sfx_axe_on_metal = "mus_sfx_axe_on_metal";
+
+    //PROPERTY mus_sfx_blazing_fire
+    public const string mus_sfx_blazing_fire = "mus_sfx_blazing_fire";
+
+    //PROPERTY mus_sfx_blazing_fire_alt_1
+    public const string mus_sfx_blazing_fire_alt_1 = "mus_sfx_blazing_fire_alt_1";
+
+    //PROPERTY mus_sfx_blazing_fire_alt_2
+    public const string mus_sfx_blazing_fire_alt_2 = "mus_sfx_blazing_fire_alt_2";
+
+    //PROPERTY mus_sfx_branch_chop_loop
+    public const string mus_sfx_branch_chop_loop = "mus_sfx_branch_chop_loop";
+
+    //PROPERTY mus_sfx_branch_chop_single
+    public const string mus_sfx_branch_chop_single = "mus_sfx_branch_chop_single";
+
+    //PROPERTY mus_sfx_chicken_clucking
+    public const string mus_sfx_chicken_clucking = "mus_sfx_chicken_clucking";
+
+    //PROPERTY mus_sfx_chickens_clucking_long
+    public const string mus_sfx_chickens_clucking_long = "mus_sfx_chickens_clucking_long";
+
+    //PROPERTY mus_sfx_chickens_clucking_short
+    public const string mus_sfx_chickens_clucking_short = "mus_sfx_chickens_clucking_short";
+
+    //PROPERTY mus_sfx_crash1
+    public const string mus_sfx_crash1 = "mus_sfx_crash1";
+
+    //PROPERTY mus_sfx_creek_loop
+    public const string mus_sfx_creek_loop = "mus_sfx_creek_loop";
+
+    //PROPERTY mus_sfx_creek_loop_long
+    public const string mus_sfx_creek_loop_long = "mus_sfx_creek_loop_long";
+
+    //PROPERTY mus_sfx_dogs_baying
+    public const string mus_sfx_dogs_baying = "mus_sfx_dogs_baying";
+
+    //PROPERTY mus_sfx_door_close
+    public const string mus_sfx_door_close = "mus_sfx_door_close";
+
+    //PROPERTY mus_sfx_door_open
+    public const string mus_sfx_door_open = "mus_sfx_door_open";
+
+    //PROPERTY mus_sfx_hogs_squealing
+    public const string mus_sfx_hogs_squealing = "mus_sfx_hogs_squealing";
+
+    //PROPERTY mus_sfx_hogs_squealing_short
+    public const string mus_sfx_hogs_squealing_short = "mus_sfx_hogs_squealing_short";
+
+    //PROPERTY mus_sfx_horses_gallop
+    public const string mus_sfx_horses_gallop = "mus_sfx_horses_gallop";
+
+    //PROPERTY mus_sfx_horses_gallop_alt
+    public const string mus_sfx_horses_gallop_alt = "mus_sfx_horses_gallop_alt";
+
+    //PROPERTY mus_sfx_horses_gallop3
+    public const string mus_sfx_horses_gallop3 = "mus_sfx_horses_gallop3";
+
+    //PROPERTY mus_sfx_horses_trot
+    public const string mus_sfx_horses_trot = "mus_sfx_horses_trot";
+
+    //PROPERTY mus_sfx_horses_trot_alt
+    public const string mus_sfx_horses_trot_alt = "mus_sfx_horses_trot_alt";
+
+    //PROPERTY mus_sfx_laundry_paddle_loop
+    public const string mus_sfx_laundry_paddle_loop = "mus_sfx_laundry_paddle_loop";
+
+    //PROPERTY mus_sfx_laundry_paddle_single
+    public const string mus_sfx_laundry_paddle_single = "mus_sfx_laundry_paddle_single";
+
+    //PROPERTY mus_sfx_plantation_map_nosong
+    public const string mus_sfx_plantation_map_nosong = "mus_sfx_plantation_map_nosong";
+
+    //PROPERTY mus_sfx_plantation_map_song
+    public const string mus_sfx_plantation_map_song = "mus_sfx_plantation_map_song";
+
+    //PROPERTY mus_sfx_pour_slop
+    public const string mus_sfx_pour_slop = "mus_sfx_pour_slop";
+
+    //PROPERTY mus_sfx_quarters_ambient_morning
+    public const string mus_sfx_quarters_ambient_morning = "mus_sfx_quarters_ambient_morning";
+
+    //PROPERTY mus_sfx_quarters_ambient_morning_alt_1
+    public const string mus_sfx_quarters_ambient_morning_alt_1 = "mus_sfx_quarters_ambient_morning_alt_1";
+
+    //PROPERTY mus_sfx_river_flowing
+    public const string mus_sfx_river_flowing = "mus_sfx_river_flowing";
+
+    //PROPERTY mus_sfx_running_underbrush
+    public const string mus_sfx_running_underbrush = "mus_sfx_running_underbrush";
+
+    //PROPERTY mus_sfx_smokehouse_close_1
+    public const string mus_sfx_smokehouse_close_1 = "mus_sfx_smokehouse_close_1";
+
+    //PROPERTY mus_sfx_smokehouse_close_2
+    public const string mus_sfx_smokehouse_close_2 = "mus_sfx_smokehouse_close_2";
+
+    //PROPERTY mus_sfx_smokehouse_open
+    public const string mus_sfx_smokehouse_open = "mus_sfx_smokehouse_open";
+
+    //PROPERTY mus_sfx_smokehouse_task
+    public const string mus_sfx_smokehouse_task = "mus_sfx_smokehouse_task";
+
+    //PROPERTY mus_sfx_stairs_coming
+    public const string mus_sfx_stairs_coming = "mus_sfx_stairs_coming";
+
+    //PROPERTY mus_sfx_stairs_going
+    public const string mus_sfx_stairs_going = "mus_sfx_stairs_going";
+
+    //PROPERTY mus_sfx_steamboat
+    public const string mus_sfx_steamboat = "mus_sfx_steamboat";
+
+    //PROPERTY mus_sfx_task_completed
+    public const string mus_sfx_task_completed = "mus_sfx_task_completed";
+
+    //PROPERTY mus_sfx_task_icon_appears
+    public const string mus_sfx_task_icon_appears = "mus_sfx_task_icon_appears";
+
+    //PROPERTY mus_sfx_tutorial_appear
+    public const string mus_sfx_tutorial_appear = "mus_sfx_tutorial_appear";
+
+    //PROPERTY mus_sfx_wagon_wheels
+    public const string mus_sfx_wagon_wheels = "mus_sfx_wagon_wheels";
+
+    //PROPERTY mus_sfx_water_pour
+    public const string mus_sfx_water_pour = "mus_sfx_water_pour";
+
+    //PROPERTY mus_sfx_water_pour_short
+    public const string mus_sfx_water_pour_short = "mus_sfx_water_pour_short";
+
+    //PROPERTY mus_sfx_work_bell
+    public const string mus_sfx_work_bell = "mus_sfx_work_bell";
+
+    //PROPERTY mus_sfx_work_song
+    public const string mus_sfx_work_song = "mus_sfx_work_song";
+
+    //PROPERTY mus_sfx_yard_ambient_day1
+    public const string mus_sfx_yard_ambient_day1 = "mus_sfx_yard_ambient_day1";
+
+    //PROPERTY mus_sfx_yard_ambient_song
+    public const string mus_sfx_yard_ambient_song = "mus_sfx_yard_ambient_song";
+
+    //PROPERTY musbell_alt_1
+    public const string musbell_alt_1 = "musbell_alt_1";
+
+    //PROPERTY p3_jonah_transition
+    public const string p3_jonah_transition = "p3_jonah_transition";
+
+    //PROPERTY p3_tcb_001_n18_p0
+    public const string p3_tcb_001_n18_p0 = "p3_tcb_001_n18_p0";
+
+    //PROPERTY ripley_day
+    public const string ripley_day = "ripley_day";
+
+    //PROPERTY running_underbrush
+    public const string running_underbrush = "running_underbrush";
 
     //PROPERTY savegame_autosave
     public const string savegame_autosave = "savegame_autosave";
@@ -69,7 +279,7 @@ public partial class AudioIds {
     //PROPERTY savegame_prologue_start
     public const string savegame_prologue_start = "savegame_prologue_start";
 
-    //PROPERTY long_john_loop
-    public const string long_john_loop = "long_john_loop";
+    //PROPERTY steamboat
+    public const string steamboat = "steamboat";
 }
 //CLASS_END AudioIds
